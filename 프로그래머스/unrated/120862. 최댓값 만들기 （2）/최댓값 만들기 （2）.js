ebe7d87c9180ef78fd806141bedadf795b4e1932
saem <95661930+kor-sams-dev@users.noreply.github.com>
2022-10-12 08:50:35 +0900

    [unrated] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,5 @@
+function solution(numbers) {
+    numbers.sort((a,b) => a-b)
+    
+    return numbers[0] * numbers[1] > numbers[numbers.length-1] * numbers[numbers.length-2] ? numbers[0] * numbers[1] : numbers[numbers.length-1] * numbers[numbers.length-2]
+}
