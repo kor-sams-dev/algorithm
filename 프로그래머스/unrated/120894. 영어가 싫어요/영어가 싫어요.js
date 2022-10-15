79ab98d1acb3cb7815c6009bd8b2c53afa39968d
saem <95661930+kor-sams-dev@users.noreply.github.com>
2022-10-15 16:48:11 +0900

    [unrated] Title: 영어가 싫어요, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,20 +1,8 @@
 function solution(numbers) {
-    const numbersString = [
-        {string: "zero", value: 0}, 
-        {string: "seven", value: 7},
-        {string: "three", value: 3},
-        {string: "eight", value: 8},
-        {string: "nine", value: 9},
-        {string: "four", value: 4},
-        {string: "five", value: 5},
-        {string: "one", value: 1},
-        {string: "two", value: 2},
-        {string: "six", value: 6}
-    ];
+    const numbersString = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     
-    return parseInt(numbersString.reduce((a,b)=>{
-        const re = new RegExp(b.string,'g')
-        return a.replace(re, b.value)    
+    return parseInt(numbersString.reduce((a,b,index)=>{
+        return a.replace(new RegExp(b,'g'), index)    
     },numbers))
     
 }
