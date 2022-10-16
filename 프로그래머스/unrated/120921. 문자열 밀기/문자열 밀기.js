cabf08c69c48609b61e84b209bd41245b1b1bebb
Tom <95661930+kor-sams-dev@users.noreply.github.com>
2022-10-16 19:16:28 +0900

    [unrated] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
@@ -1,4 +1,4 @@
 function solution(A, B) {
-    const index = (A + A).indexOf(B)
-    return index === -1 ? -1 : (A.length - index) % A.length
+    const index = (B + B).indexOf(A)
+    return index 
 }
