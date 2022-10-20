bace21945b645691e415e56e8ea19c9c02adf816
Tom <95661930+kor-sams-dev@users.noreply.github.com>
2022-10-20 15:44:09 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 9528 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var input = parseInt(fs.readFileSync('/dev/stdin').toString());
+
+for(let i = 1; i <= input; i++){
+    console.log(`${' '.repeat(input - i)}${'*'.repeat(i)}`)
+}
