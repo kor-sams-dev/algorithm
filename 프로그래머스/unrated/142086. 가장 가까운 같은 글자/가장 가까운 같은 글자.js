e46b5ca01aec45aa8bd44a8e96b097408a4445ca
Tom <95661930+kor-sams-dev@users.noreply.github.com>
2022-12-14 11:32:30 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 1.88 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,17 +1,7 @@
 function solution(s) {
-    // return s.split('').reduce((acc, cur, index)=> {
-    //     acc.result.push(acc.word[cur] ? index - acc.word[cur] : -1)
-    //     acc.word[cur] = index
-    //     return acc
-    // }, {result:[], word:{}}).result
-    const result = []
-    const obj = {}
-    
-    for(let i = 0; i < s.length; i++){
-        let leng = obj[s[i]] === undefined ? -1 : i - obj[s[i]];
-        obj[s[i]] = i
-        result.push(leng)
-    }
-    
-    return result
+    return s.split('').reduce((acc, cur, index)=> {
+        acc.result.push(acc.word[cur] === undefined ? -1 : index - acc.word[cur])
+        acc.word[cur] = index
+        return acc
+    }, {result:[], word:{}}).result
 } 
